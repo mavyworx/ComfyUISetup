39d98f08655b9442e01ea85b8be3dbae7c69ba98
mavyworx <ryan@mavyworx.com>
2025-05-29 18:23:19 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,4 @@ if [ ! -d "$COMFY_DIR" ]; then
   
   apt update
   apt install -y wget curl git ca-certificates
-
-  MINICONDA_DIR=/workspace/miniconda
-  mkdir -p $MINICONDA_DIR
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $MINICONDA_DIR/miniconda.sh
-  bash $MINICONDA_DIR/miniconda.sh -b -u -p $MINICONDA_DIR
-  rm $MINICONDA_DIR/miniconda.sh
-  source $MINICONDA_DIR/bin/activate
-  conda create -y -n comfyenv python=3.12 pytorch=2.7.* pytorch-cuda=12.4 -c pytorch -c nvidia -c conda-forge
 fi
