280be590e40133a627bbbbfc77a0c1bfee838eda
mavyworx <ryan@mavyworx.com>
2025-06-01 15:10:16 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set PS4=''
 
-echo "ComfyUI Setup Script starting..."
+: "[ComfyUISetup] setup script starting..."
 
 set -xeuo pipefail
 
@@ -9,20 +8,20 @@ COMFY_DIR="/workspace/ComfyUI"
 CONDA_ENV_DIR="/workspace/comfyenv"
 
 # One-time setup ────────────────────────────────────────
-if [ ! -d "$COMFY_DIR" ]; then
-  echo "$COMFY_DIR not found, installing ComfyUI..."
-  
-  # Conda environment
+if [ ! -d $CONDA_ENV_DIR ]; then
+  : "[ComfyUISetup] No Conda environment found, creating..."
   conda create -y -p $CONDA_ENV_DIR python=3.12 
   conda install -y -p $CONDA_ENV_DIR pytorch=2.6.* pytorch-cuda=12.4 torchvision torchaudio -c pytorch -c nvidia -c conda-forge
+fi
 
-  # ComfyUI
+if [ ! -d "$COMFY_DIR" ]; then
+  : "[ComfyUISetup] No ComfyUI found, creating..."
   git clone --depth 1 https://github.com/comfyanonymous/ComfyUI.git $COMFY_DIR
   cd $COMFY_DIR
   conda run -p $CONDA_ENV_DIR pip install -r requirements.txt
-
-  sleep infinity
 fi
 
+: "[ComfyUISetup] starting ComfyUI..."
 cd $COMFY_DIR
 conda run -p $CONDA_ENV_DIR python main.py
+sleep infinity
