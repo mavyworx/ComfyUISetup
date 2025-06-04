d23dce7d2f2dc6a0d455b2ec24e465d95d4939cc
mavyworx <ryan@mavyworx.com>
2025-06-03 20:59:31 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,7 @@ trap "echo [ComfyUISetup] script encountered an error. exiting without restart; 
 : "[ComfyUISetup] setup script starting..."
 
 COMFY_DIR="/workspace/ComfyUI"
-COMFY_MGR_DIR="/workspace/ComfyUI/custom_nodes/comfyui-manager"
 COMFY_ENV_DIR="/workspace/comfyenv"
-OUTPUT_DIR="/workspace/output"
 
 if [ ! -d $COMFY_ENV_DIR ]; then
   : "[ComfyUISetup] No Conda environment found, creating..."
@@ -23,11 +21,21 @@ if [ ! -d $COMFY_DIR ]; then
   conda run -p $COMFY_ENV_DIR pip install -r requirements.txt
 fi
 
+COMFY_MGR_DIR=$COMFY_DIR/custom_nodes/comfyui-manager"
 if [ ! -d $COMFY_MGR_DIR ]; then
   : "[ComfyUISetup] No ComfyUI Manager found, creating..."
   git clone --depth 1 https://github.com/ltdrdata/ComfyUI-Manager $COMFY_MGR_DIR
 fi
 
+CRYSTOOLS_DIR=$COMFY_DIR/custom_nodes/ComfyUI-Crystools
+if [ ! -d $CRYSTOOLS_DIR ]; then
+  : "[ComfyUISetup] No ComfyUI-Crystools found, creating..."
+  git clone --depth 1 https://github.com/crystian/ComfyUI-Crystools.git
+  cd $CRYSTOOLS_DIR
+  conda run -p $COMFY_ENV_DIR pip install -r requirements.txt
+fi
+
+OUTPUT_DIR="/workspace/output"
 if [ ! -d $OUTPUT_DIR ]; then
   mkdir $OUTPUT_DIR
 fi
